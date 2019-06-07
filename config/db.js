6265f1f2689ac9317b9971e00d53b1cb715c46bb
MigueMartelo <miguemartelo1031@gmail.com>
2019-06-07 14:59:37 -0500

    Trabajando con DB by Clever Cloud

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('uptasknode', 'root', 'root@2018', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: '3306',
-  operatorAliases: false,
-  define: {
-    timestamps: false
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+const sequelize = new Sequelize('b0ozadgvibfaqf4bz3n5', 'ul0m3zirp3lew07h', '1iTgcxMjxXUz8Fl830kl', {
+	host: 'b0ozadgvibfaqf4bz3n5-mysql.services.clever-cloud.com',
+	dialect: 'mysql',
+	port: '3306',
+	operatorAliases: false,
+	define: {
+		timestamps: false
+	},
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
 });
 
 module.exports = sequelize;
