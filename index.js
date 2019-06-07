79660393fb48d41acd04047cf654d9418e2a55ea
MigueMartelo <miguemartelo1031@gmail.com>
2019-06-07 16:50:40 -0500

    Permitir a los usuarios crear sus cuentas

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 // importando helpers con algunas funciones
 const helpers = require('./helpers');
@@ -12,10 +16,12 @@ const db = require('./config/db');
 // Importar el modelo
 require('./models/Proyectos');
 require('./models/Tareas');
+require('./models/Usuarios');
 
-db.sync()
-  .then(() => console.log('Conectado al Servidor'))
-  .catch(err => console.log(err));
+db
+	.sync()
+	.then(() => console.log('Conectado al Servidor'))
+	.catch(err => console.log(err));
 
 // crear una aplicación de express
 const app = express();
@@ -23,20 +29,38 @@ const app = express();
 // donde cargar archivos estaticos
 app.use(express.static('public'));
 
+// habilitar bodyParser para leer datos de formularios
+app.use(bodyParser.urlencoded({ extends: true }));
+
+// agregamos express validator a toda la aplicación
+app.use(expressValidator());
+
 // habilitar pug
 app.set('view engine', 'pug');
 
 // añadir carpeta de las vistas
 app.set('views', path.join(__dirname, './views'));
 
+// agregar flash messages
+app.use(flash());
+
+app.use(cookieParser());
+
+// sessiones nos permiten navegar entre distintas paginas sin volver autenticarnos
+app.use(
+	session({
+		secret: 'supersecreto',
+		resave: false,
+		saveUninitialized: false
+	})
+);
+
 // habilitando vardump en la aplicación
 app.use((req, res, next) => {
-  res.locals.vardump = helpers.vardump;
-  next();
+	res.locals.vardump = helpers.vardump;
+	res.locals.mensajes = req.flash();
+	next();
 });
-
-// habilitar bodyParser para leer datos de formularios
-app.use(bodyParser.urlencoded({ extends: true }));
 
 // rutas
 app.use('/', routes());
--- routes/index.js
+++ routes/index.js
@@ -4,38 +4,55 @@ const router = express.Router();
 // importar express-validator
 const { body } = require('express-validator/check');
 
-// importar controllador
+// importar controladores
 const proyectosController = require('../controllers/proyectosController');
 const tareasController = require('../controllers/tareasController');
+const usuariosController = require('../controllers/usuariosController');
 
-module.exports = function () {
-  // ruta para el home
-  router.get('/', proyectosController.proyectosHome);
-  router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
-  router.post('/nuevo-proyecto',
-    body('nombre').not().isEmpty().trim().escape(),
-    proyectosController.nuevoProyecto);
+module.exports = function() {
+	// ruta para el home
+	router.get('/', proyectosController.proyectosHome);
+	router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
+	router.post(
+		'/nuevo-proyecto',
+		body('nombre')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		proyectosController.nuevoProyecto
+	);
 
-  // Listar proyecto
-  router.get('/proyectos/:url', proyectosController.proyectoPorUrl);
+	// Listar proyecto
+	router.get('/proyectos/:url', proyectosController.proyectoPorUrl);
 
-  // Editar proyecto
-  router.get('/proyecto/editar/:id', proyectosController.formularioEditar);
-  router.post('/nuevo-proyecto/:id',
-    body('nombre').not().isEmpty().trim().escape(),
-    proyectosController.actualizarProyecto);
+	// Editar proyecto
+	router.get('/proyecto/editar/:id', proyectosController.formularioEditar);
+	router.post(
+		'/nuevo-proyecto/:id',
+		body('nombre')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		proyectosController.actualizarProyecto
+	);
 
-  // Eliminar proyecto
-  router.delete('/proyectos/:url', proyectosController.eliminarProyecto);
+	// Eliminar proyecto
+	router.delete('/proyectos/:url', proyectosController.eliminarProyecto);
 
-  // Tareas
-  router.post('/proyectos/:url', tareasController.agregarTarea);
+	// Tareas
+	router.post('/proyectos/:url', tareasController.agregarTarea);
 
-  // Actualizar tarea
-  router.patch('/tareas/:id', tareasController.cambiarEstadoTarea);
+	// Actualizar tarea
+	router.patch('/tareas/:id', tareasController.cambiarEstadoTarea);
 
-  // Eliminar tarea
-  router.delete('/tareas/:id', tareasController.eliminarTarea);
+	// Eliminar tarea
+	router.delete('/tareas/:id', tareasController.eliminarTarea);
 
-  return router;
-}
+	// Crear nueva cuenta
+	router.get('/crear-cuenta', usuariosController.formCrearCuenta);
+	router.post('/crear-cuenta', usuariosController.crearCuenta);
+
+	return router;
+};
